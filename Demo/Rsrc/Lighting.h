b4904255ca41768eb509f8d4af1598da01dc513d
Irlan Robson <dnairlan@gmail.com>
2015-09-01 16:25:14 -0300

    Update Testbed

--- Demo/Rsrc/Lighting.h
+++ Demo/Rsrc/Lighting.h
@@ -8,7 +8,7 @@ struct LIGHT_ARGS {
 	float fPower;
 };
 
-void BlinnPhongPoint( in LIGHT_ARGS _laLightArgs, inout float3 _vDiffuse, inout float3 _vSpecular ) {
-	_vDiffuse += ( _laLightArgs.fLdotN * _laLightArgs.vLightDiffuse ) * (_laLightArgs.fPower / _laLightArgs.fDistance);
-	_vSpecular += ( pow(_laLightArgs.fHdotN, _laLightArgs.fShininess) * _laLightArgs.vLightSpecular ) / _laLightArgs.fDistance;
+void BlinnPhongDir( in LIGHT_ARGS _laLightArgs, inout float3 _vDiffuse, inout float3 _vSpecular ) {
+	_vDiffuse += _laLightArgs.fLdotN * _laLightArgs.vLightDiffuse;
+	_vSpecular += pow(_laLightArgs.fHdotN, _laLightArgs.fShininess) * _laLightArgs.vLightSpecular;
 }
