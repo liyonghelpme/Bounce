f31a3c7e25039e268bb7ca67f79f32a56d107868
Irlan Robson <dnairlan@gmail.com>
2015-09-04 12:19:23 -0300

    Update Testbed
    Remove .hlsl include .cso

--- Demo/Rsrc/ColorVs.hlsl
+++
@@ -1,9 +0,0 @@
-#include "Engine.h"
-
-void main( in IN_VECS _vIn, out OUT_VECS _vOut, out float4 _vOutPos : SV_POSITION ) {
-	// Assuming Uniform Scaled World Matrix
-	_vOut.vViewPos = mul(g_euEngineUniforms.mWorldView, float4(_vIn.vPos, 1.0));
-	_vOut.vViewNormal = mul(g_euEngineUniforms.mWorldView, float4(_vIn.vNormal, 0.0));
-	_vOut.vTexCoord = _vIn.vTexCoord;
-	_vOutPos = mul(g_euEngineUniforms.mWorldViewProj, float4(_vIn.vPos, 1.0));
-}
--- Demo/Rsrc/DebugPs.hlsl
+++
@@ -1,5 +0,0 @@
-#include "Engine.h"
-
-void main( in OUT_VECS _ovInVecs, out float4 _vOutColor : SV_TARGET ) {
-	_vOutColor = g_euEngineUniforms.vDiffuseMaterial;
-}
